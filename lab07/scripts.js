4823c7da0a93bd9c9b0f7297079597d8eebceef8
65160370 <156596189+65160370-bam@users.noreply.github.com>
2024-01-17 19:53:09 +0700

    เพิ่มรายการ todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -19,17 +19,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 }
 
-    // ลบรายการ Todo
-    function deleteTodo(index) {
-    todos.splice(index, 1);
-    renderTodoList();
-    }
+    // // ลบรายการ Todo
+    // function deleteTodo(index) {
+    // todos.splice(index, 1);
+    // renderTodoList();
+    // }
 
-    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
-    function toggleComplete(index) {
-    todos[index].completed = !todos[index].completed;
-    renderTodoList();
-    }
+    // // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    // function toggleComplete(index) {
+    // todos[index].completed = !todos[index].completed;
+    // renderTodoList();
+    // }
 
 // แสดงรายการ Todo บนหน้าเว็บ
 function renderTodoList() {
